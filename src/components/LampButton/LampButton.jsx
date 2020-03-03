74c0595a4b84a861441f8d09af20367474a1e46a
Higor Menezes <higormenezes1996@gmail.com>
2020-03-03 19:24:54 -0300

    add aria-label on button lamp

--- src/components/LampButton/LampButton.jsx
+++ src/components/LampButton/LampButton.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { Button, Lamp } from "./styles";
 
 const LampButton = ({ onClick, theme, ...rest }) => (
-  <Button onClick={onClick} className={theme} {...rest}>
+  <Button onClick={onClick} className={theme} aria-label="Lamp" {...rest}>
     <Lamp />
   </Button>
 );
