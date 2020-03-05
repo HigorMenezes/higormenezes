f7ce9b08a8bb164ed9b6ff16dfc3e970b55cb792
Higor Menezes <higormenezes1996@gmail.com>
2020-03-04 23:01:29 -0300

    add feeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,5 +103,57 @@ module.exports = {
         trackingId: "UA-159641441-1",
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => ({
+                ...edge.node.frontmatter,
+                description: edge.node.frontmatter.spoiler,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [{ "content:encoded": edge.node.html }],
+              }));
+            },
+            query: `
+              {
+                allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {fields: {langKey: {eq: "pt-br"}}}) {
+                  edges {
+                    node {
+                      html
+                      fields {
+                        slug
+                      }
+                      frontmatter {
+                        title
+                        date
+                        spoiler
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: `/rss.xml`,
+            title: `Higor Menezes's Blog RSS Feed`,
+          },
+        ],
+      },
+    },
   ],
 };
